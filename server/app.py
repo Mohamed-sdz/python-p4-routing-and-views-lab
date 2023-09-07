fc12568292d30ef198bce91f817182ff5470fc35
saiderrahmani <didi.mousedidi@outlook.com>
2023-09-06 23:51:16 -0400

    create app.py

--- server/app.py
+++ server/app.py
@@ -1,8 +1,35 @@
-#!/usr/bin/env python3
-
 from flask import Flask
+ 
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<text>')
+def print_text(text):
+    print(text)
+    return text
+
+@app.route('/count/<int:num>')
+def count(num):
+    count = ''
+    for i in range(num):
+        count += str(i) + '\n'
+    return count
+@app.route('/math/<int:a>/<op>/<int:b>')
+def math(a, op, b):
+    if op == '+':
+        return str(a + b)
+    elif op == '-':
+        return str(a - b)
+    elif op == '*':
+        return str(a * b)
+    elif op == 'div':
+        return str(a / b)
+    elif op == '%':
+        return str(a % b)
+        
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
